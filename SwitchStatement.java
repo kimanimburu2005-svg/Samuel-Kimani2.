0ad83e3c60eeb8ab7b79413875b2771bb9c54272
Samuel Mburu <kimanimburu2005@gmail.com>
2026-02-18 20:27:35 +0300

    Implement switch statement for day name lookup
    This program demonstrates the use of a switch statement to determine the name of a day based on user input.

--- SwitchStatement.java
+++ SwitchStatement.java
@@ -1,1 +1,50 @@
+import java.util.Scanner;
 
+public class SwitchStatement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("=====================================");
+        System.out.println("SWITCH STATEMENT DEMONSTRATION");
+        System.out.println("=====================================");
+        System.out.println("Student: Samuel Kimani");
+        System.out.println();
+
+        System.out.print("Enter a number between 1 and 7: ");
+        int dayNumber = input.nextInt();
+
+        String dayName;
+        System.out.println();
+
+        switch (dayNumber) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid input! Please enter a number between 1 and 7.";
+                break;
+        }
+
+        System.out.println("Day " + dayNumber + ": " + dayName);
+        System.out.println("\n=====================================");
+        input.close();
+    }
+}
